3ea4818de28620e233d70f5b329612c11b3597d0
Gener Echeverria <generalex@live.com.mx>
2020-09-29 11:35:04 -0500

    Tareas

---
+++ TAREAS/JavaApplication18/src/Paquete/Auto.java
@@ -0,0 +1,16 @@
+
+package Paquete;
+
+/**
+ *
+ * @author gener_000
+ */
+public class Auto {
+    private String color;
+    private int numeroPuertas;
+    private int numeroLLantas;
+    
+    public void acelerarAuto (){
+        System.out.println("Acelerando");
+    }
+}
---
+++ TAREAS/JavaApplication18/src/Paquete/CreacionDeAutos.java
@@ -0,0 +1,20 @@
+
+package Paquete;
+
+/**
+ *
+ * @author gener_000
+ */
+public class CreacionDeAutos {
+
+    public static void main(String[] args) {
+       Auto tracker =  new Auto();
+       tracker.acelerarAuto();
+       
+       
+       
+       
+       
+    }
+    
+}
